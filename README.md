5a7e72bf5a52efb0845a52d025bf3869fb887d65
doout <baheer-9@hotmail.com>
2017-04-12 17:24:39 -0400

    Added testcase

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# CPS506
+Name:   Baheer Kamal,Alessandro Profenna
+Course: CPS506, Winter 2017, Assignment #4
+Due:    2017.01.14 08:30
+Credit: This is entirely my/our own work.
--- src/main.rs
+++ src/main.rs
@@ -64,13 +64,15 @@ struct Game {
 }
 
 fn main() {
-    let b = "board 3 4\nplayers 2\ndice 1 2 2 2 2\nturns 5\n".to_string();
+    /*let b = "board 3 4\nplayers 2\ndice 1 2 2 2 2\nturns 5\n".to_string();
     let b = "board 3 4\nplayers 2\ndice 1 2\nturns 5".to_string();
     let b = "board 3 4\nplayers 2\ndice 1 2 2 2 2\nladder 5 11\nsnake 8 4\nturns 5".to_string();
     let b = "board 3 4\nplayers 2\ndice 1 2 2 2 2\nladder 5 11\nsnake 8 4\npowerup escalator 6 9\npowerup antivenom 7\npowerup double 4\nturns 10".to_string();
-    // let b = read_commands();
+   */
+    let b = read_commands();
     let game = readFrom(b);
-    print_board(&game);
+    println!("{}", game.as_string());
+    // print_board(&game);
 }
 
 
@@ -81,6 +83,10 @@ fn make_emtpy_map() -> Game {
         tiles: (Vec::new()),
         dice: Dice { dices: vec![1], index: 0 }
     };
+}
+
+fn print(game: &Game) -> String {
+    return game.as_string();
 }
 
 #[allow(non_snake_case)]
@@ -136,13 +142,13 @@ fn readFrom(read: String) -> Game {
 
 fn player_one_around(mut map: Game) -> Game {
     for index in 0..map.players.len() {
-        if get_player_on_tile(&map, map.board.size) != -1 { return map; }
+        if map.get_player_on_tile(map.board.size) != -1 { return map; }
         let mut roll = map.dice.dices[map.dice.index];
         map.dice.index = (map.dice.index + 1) % map.dice.dices.len();
         if map.players[index].have_powerup(3) { roll = roll * 2; }
         map.players[index].remove_powerup(3);
         let new_loc = map.players[index].location + roll as i32;
-       // println!("{} : {} : {:b}", map.players[index].name, roll, map.players[index].powerup);
+        // println!("{} : {} : {:b}", map.players[index].name, roll, map.players[index].powerup);
         map = move_player(map, index as i8, new_loc); // check make it roll 1
     }
     return map;
@@ -152,7 +158,7 @@ fn player_one_around(mut map: Game) -> Game {
 fn move_player(mut map: Game, player_index: i8, mut new_player_locatin: i32) -> Game {
     let player_index = player_index as usize;
     if new_player_locatin > map.board.size { return map; }
-    let specal_tiles = get_specal_tile_index(&map, new_player_locatin);
+    let specal_tiles = map.get_specal_tile_index(new_player_locatin);
     if specal_tiles != -1 {
         let ref tile = map.tiles[specal_tiles as usize];
         if tile.displacement != -1 {
@@ -174,13 +180,13 @@ fn move_player(mut map: Game, player_index: i8, mut new_player_locatin: i32) -> 
         map.players[player_index].add_powerup(tile.powerup);
     }
     if new_player_locatin > map.board.size { new_player_locatin = map.board.size; }
-    let player_on_tiles = get_player_on_tile(&map, new_player_locatin);
+    let player_on_tiles = map.get_player_on_tile(new_player_locatin);
     map.players[player_index].location = new_player_locatin;
     if player_on_tiles != -1 {
         if player_on_tiles != player_index as i8 {
             return move_player(map, player_on_tiles, new_player_locatin + 1)
         } else {
-            let specal_tiles = get_specal_tile_index(&map, new_player_locatin);
+            let specal_tiles = map.get_specal_tile_index(new_player_locatin);
             if specal_tiles != -1 {
                 map.players[player_index].add_powerup(map.tiles[specal_tiles as usize].powerup);
             }
@@ -189,96 +195,123 @@ fn move_player(mut map: Game, player_index: i8, mut new_player_locatin: i32) -> 
     return map;
 }
 
-fn get_player_on_tile(map: &Game, loc: i32) -> i8 {
-    for index in 0..map.players.len() {
-        if map.players[index].location == loc {
-            return index as i8;
-        }
-    }
-    return -1;
-}
-
-fn get_specal_tile_index(map: &Game, loc: i32) -> i32 {
-    for index in 0..map.tiles.len() {
-        if map.tiles[index].location == loc {
-            return index as i32;
-        }
-    }
-    return -1;
-}
-
 fn read_commands() -> String {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer);
     return buffer;
 }
 
-fn print_board(map: &Game) {
-    let height = map.board.height;
-    let width = map.board.width;
-    if height == 0 || width == 0 { return; }
-    let mut index;
-    for y in 0..height {
-        println!("{}", print_board_border(width));
-        for t in 0..2 {
-            print!("|");
-            for x in 0..width {
-                if (y % 2 == 0 && height % 2 == 0) || (y % 2 == 1 && height % 2 == 1) {
-                    index = (height - 1 - y) * width + (width - 1 - x);
-                } else {
-                    index = (height - 1 - y) * width + x;
-                }
-                if t == 0 {
-                    print!("{}|", format_number(index + 1));
-                } else {
-                    print!("{}|", format_tile_data(map, index + 1));
-                }
+impl Game {
+    fn get_player_on_tile(&self, loc: i32) -> i8 {
+        for index in 0..self.players.len() {
+            if self.players[index].location == loc {
+                return index as i8;
             }
-            println!("");
         }
+        return -1;
     }
-    println!("{}", print_board_border(width));
-    let player_won_index = get_player_on_tile(map, map.board.size);
-    if player_won_index != -1 { println!("Player {} won", map.players[player_won_index as usize].name) }
-}
 
-fn print_board_border(w: i32) -> String {
-    let mut s = "+".to_string();
-    for v in 0..w {
-        s.push_str("---+");
-    }
-    return s;
-}
-
-fn format_tile_data(map: &Game, i: i32) -> String {
-    let mut name = ' ';
-    let mut powerup = ' ';
-    let mut dis = ' ';
-    let x = get_player_on_tile(map, i);
-    if x != -1 { name = map.players[x as usize].name; }
-    let x = get_specal_tile_index(map, i);
-    if x != -1 {
-        let ref tile = map.tiles[x as usize];
-        if tile.displacement != -1 {
-            if tile.displacement > tile.location { dis = 'L' } else { dis = 'S' }
+    fn get_specal_tile_index(&self, loc: i32) -> i32 {
+        for index in 0..self.tiles.len() {
+            if self.tiles[index].location == loc {
+                return index as i32;
+            }
         }
-        match tile.powerup {
-            1 => powerup = 'e',
-            2 => powerup = 'a',
-            3 => powerup = 'd',
-            _ => powerup = ' ',
+        return -1;
+    }
+
+    fn as_string(&self) -> String {
+        let height = self.board.height;
+        let width = self.board.width;
+        let mut out = String::new();
+        if height == 0 || width == 0 { return out; }
+        let mut index;
+        for y in 0..height {
+            out.push_str(Game::print_board_border(width).as_str());
+            out.push('\n');
+            for t in 0..2 {
+                for x in 0..width {
+                    if (y % 2 == 0 && height % 2 == 0) || (y % 2 == 1 && height % 2 == 1) {
+                        index = (height - 1 - y) * width + (width - 1 - x);
+                    } else {
+                        index = (height - 1 - y) * width + x;
+                    }
+                    out.push('|');
+                    if t == 0 {
+                        out.push_str(Game::format_number(index + 1).as_str());
+                    } else {
+                        out.push_str(Game::format_tile_data(self, index + 1).as_str());
+                    }
+                }
+                out.push_str("|\n");
+            }
         }
+        out.push_str(Game::print_board_border(width).as_str());
+        out.push('\n');
+        let player_won_index = self.get_player_on_tile(self.board.size);
+        if player_won_index != -1 {
+            out.push_str((format!("Player {} won\n", self.players[player_won_index as usize].name)).as_str());
+        }
+        return out;
     }
-    return format!("{}{}{}", name, powerup, dis);
+
+    fn print_board_border(w: i32) -> String {
+        let mut s = "+".to_string();
+        for v in 0..w {
+            s.push_str("---+");
+        }
+        return s;
+    }
+
+    fn format_tile_data(map: &Game, i: i32) -> String {
+        let mut name = ' ';
+        let mut powerup = ' ';
+        let mut dis = ' ';
+        let x = map.get_player_on_tile(i);
+        if x != -1 { name = map.players[x as usize].name; }
+        let x = map.get_specal_tile_index(i);
+        if x != -1 {
+            let ref tile = map.tiles[x as usize];
+            if tile.displacement != -1 {
+                if tile.displacement > tile.location { dis = 'L' } else { dis = 'S' }
+            }
+            match tile.powerup {
+                1 => powerup = 'e',
+                2 => powerup = 'a',
+                3 => powerup = 'd',
+                _ => powerup = ' ',
+            }
+        }
+        return format!("{}{}{}", name, powerup, dis);
+    }
+
+    fn format_number(i: i32) -> String {
+        let mut d = i.to_string();
+        let mut k = "".to_string();
+        while k.len() < (3 - d.len()) {
+            k.push(' ');
+        }
+        return format!("{}{}", k, d);
+    }
 }
 
-fn format_number(i: i32) -> String {
-    let mut d = i.to_string();
-    let mut k = "".to_string();
-    while k.len() < (3 - d.len()) {
-        k.push(' ');
+#[cfg(test)]
+mod test {
+    use readFrom;
+    use print;
+
+    #[test]
+    fn board_test() {
+        let b = "board 3 4\nplayers 2\ndice 1 2 2 2 2\nladder 5 11\nsnake 8 4\npowerup escalator 6 9\npowerup antivenom 7\npowerup double 4\nturns 10".to_string();
+        let game = readFrom(b);
+        let out = "+---+---+---+\n| 12| 11| 10|\n|B  |   |   |\n+---+---+---+\n|  7|  8|  9|\n| a |  S| e |\n+---+---+---+\n|  6|  5|  4|\n| e |  L|Ad |\n+---+---+---+\n|  1|  2|  3|\n|   |   |   |\n+---+---+---+\nPlayer B won\n".to_string();
+        assert!(print(&game)== out);
     }
-    return format!("{}{}", k, d);
+
+    #[test]
+    fn board_test2() {
+        let game = readFrom("board 2 3".to_string());
+        let out = "+---+---+\n|  5|  6|\n|   |   |\n+---+---+\n|  4|  3|\n|   |   |\n+---+---+\n|  1|  2|\n|   |   |\n+---+---+\n";
+        assert!(print(&game) == out.to_string());
+    }
 }
-
-
